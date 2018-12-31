d0c402566220c93779410ed9004db38119e9675f
pdelboca <patriciodelboca@gmail.com>
2018-12-31 17:04:22 -0300

    Finalizada la comparacion entre ultimos cargos relevados y el actual.

--- funcionarios_tribunal_de_cuentas_provincia/scrape.py
+++ funcionarios_tribunal_de_cuentas_provincia/scrape.py
@@ -2,18 +2,6 @@ from bs4 import BeautifulSoup
 import requests
 import datetime
 import pandas as pd
-from pathlib import Path
-import os
-
-
-class Funcionario:
-    def __init__(self, cargo, fecha, nombre):
-        self.cargo = cargo
-        self.fecha = fecha
-        self.nombre = nombre    
-
-    def __str__(self):
-        return "A la fecha {}, el {} es {}".format(self.fecha, self.cargo, self.nombre)
 
 
 if __name__ == "__main__":
@@ -23,7 +11,6 @@ if __name__ == "__main__":
     soup = BeautifulSoup(data, "html.parser")
 
     # Obtener Datos
-    fecha_scrapping = datetime.date.today()
     nombre_presidente = soup.select("div.white_mainbox")[0].text.strip()
     nombre_vpmayoria = soup.select("div.white_box > div.txt")[0].text.strip()
     nombre_vpminoria = soup.select("div.white_box1 > div.txt")[0].text.strip()
@@ -33,34 +20,31 @@ if __name__ == "__main__":
     nombre_pdfp = soup.select("div.greybox_item")[1].text.strip()
     nombre_fiscalia = soup.select("div.whitebox_item_solo")[2].text.strip()
 
-    # Crear Funcionarios
-    presidente = Funcionario("Presidente", fecha_scrapping, nombre_presidente)
-    print(presidente)    
-    vpmayoria = Funcionario("Vocal por la Mayoria", fecha_scrapping, nombre_vpmayoria)
-    print(vpmayoria)
-    vpminoria = Funcionario("Vocal por la Minoria", fecha_scrapping, nombre_vpminoria)
-    print(vpminoria)
-    sdfl = Funcionario("Secretaria de Fiscalizacion Legal", fecha_scrapping, nombre_sdfl)
-    print(sdfl)
-    pdfl = Funcionario("Prosecretaria de Fiscalizacion Legal", fecha_scrapping, nombre_pdfl)
-    print(pdfl)
-    sdfp = Funcionario("Secretaria de Fiscalizacion Presupuestaria", fecha_scrapping, nombre_sdfp)
-    print(sdfp)
-    pdfp = Funcionario("Prosecretaria de Fiscalizacion Presupuestaria", fecha_scrapping, nombre_pdfp)
-    print(pdfp)
-    fiscalia = Funcionario("Fiscalia", fecha_scrapping, nombre_fiscalia)
-    print(fiscalia)
-
     # Funcionarios
+    cargos = ["Presidente", "Vocal por la Mayoria", "Vocal por la Minoria", "Secretaria de Fiscalizacion Legal", "Prosecretaria de Fiscalizacion Legal", "Secretaria de Fiscalizacion Presupuestaria", "Prosecretaria de Fiscalizacion Presupuestaria", "Fiscalia"]
+
     funcionarios = dict(
-        cargo = ["Presidente", "Vocal por la Mayoria", "Vocal por la Minoria", "Secretaria de Fiscalizacion Legal", "Prosecretaria de Fiscalizacion Legal", "Secretaria de Fiscalizacion Presupuestaria", "Prosecretaria de Fiscalizacion Presupuestaria", "Fiscalia"],
-        nombre = [nombre_presidente, nombre_vpmayoria, nombre_vpminoria, nombre_sdfl, nombre_pdfl, nombre_sdfp, nombre_pdfp, fiscalia],
+        cargo = cargos,
+        nombre = [nombre_presidente, nombre_vpmayoria, nombre_vpminoria, nombre_sdfl, nombre_pdfl, nombre_sdfp, nombre_pdfp, nombre_fiscalia],
         fecha = [str(datetime.date.today())] * 8
     )
 
     # Historico de Funcionarios
-    df_historico = pd.read_csv("./data/historia_funcionarios.csv")
-    
+    df_historico = pd.read_csv("./data/historia_funcionarios.csv", parse_dates=[2])
+
     # Usando pandas creo csv y guardo datos
-    df_funcionarios = pd.DataFrame(funcionarios)
+    df_funcionarios_actuales = pd.DataFrame(funcionarios)
+
+    # Logica comparacion
+    df_ultimo_relevamiento = df_historico[df_historico.fecha == max(df_historico.fecha)]
+    df_comparacion = pd.concat([df_historico, df_funcionarios_actuales])
     
+    for cargo in cargos:
+        df_tmp = df_comparacion[df_comparacion.cargo == cargo]
+              
+        if df_tmp.iloc[0].nombre != df_tmp.iloc[1].nombre:
+            print("Entre {} y {} el cargo de {} cambio de {} a {}".format(df_tmp.iloc[0].fecha, df_tmp.iloc[1].fecha, cargo, df_tmp.iloc[0].nombre, df_tmp.iloc[1].nombre))
+        else:
+            print("Entre {} y {} el cargo {} se mantiene sin cambios.".format(df_tmp.iloc[0].fecha, df_tmp.iloc[1].fecha, cargo))
+
+    pd.concat([df_historico, df_funcionarios_actuales]).to_csv("./data/historia_funcionarios.csv", index=False)
