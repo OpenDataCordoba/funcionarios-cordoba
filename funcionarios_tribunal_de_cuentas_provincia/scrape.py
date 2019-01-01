3d0212feff5eea3097e8e1d2a6e4257d4b74931c
pdelboca <patriciodelboca@gmail.com>
2019-01-01 12:35:48 -0300

    Refactor para almacenar historico en archivos separados (mas tolerante a fallos)

--- funcionarios_tribunal_de_cuentas_provincia/scrape.py
+++ funcionarios_tribunal_de_cuentas_provincia/scrape.py
@@ -1,7 +1,27 @@
 from bs4 import BeautifulSoup
 import requests
 import datetime
+import os
 import pandas as pd
+
+def obtener_nombre_archivos_historicos():
+    """Funcion para obtener el listado de archivos csv que almacenan los cargos historicos."""    
+    archivos_historicos = []
+    root_archivos = os.path.join(".","data")
+
+    for root, dirs, files in os.walk(root_archivos):
+        for fname in files:
+            archivos_historicos.append(fname)
+            
+    return(archivos_historicos)
+
+def obtener_ultimo_relevamiento_cargos(listado_archivos):
+    """Funcion lee y devuelve el ultimo relevamiento hecho. Performance al pedo."""
+    ultimo_relevamiento = max([datetime.datetime.strptime(x, '%Y-%m-%d.csv') for x in listado_archivos])
+    path_archivo = os.path.join(".","data", ultimo_relevamiento.date().isoformat() + ".csv")
+    df_tmp = pd.read_csv(path_archivo, parse_dates=[2])
+    return df_tmp
+
 
 
 if __name__ == "__main__":
@@ -28,16 +48,14 @@ if __name__ == "__main__":
         nombre = [nombre_presidente, nombre_vpmayoria, nombre_vpminoria, nombre_sdfl, nombre_pdfl, nombre_sdfp, nombre_pdfp, nombre_fiscalia],
         fecha = [str(datetime.date.today())] * 8
     )
-
-    # Historico de Funcionarios
-    df_historico = pd.read_csv("./data/historia_funcionarios.csv", parse_dates=[2])
-
-    # Usando pandas creo csv y guardo datos
     df_funcionarios_actuales = pd.DataFrame(funcionarios)
 
+    
     # Logica comparacion
-    df_ultimo_relevamiento = df_historico[df_historico.fecha == max(df_historico.fecha)]
-    df_comparacion = pd.concat([df_historico, df_funcionarios_actuales])
+    listado_archivos = obtener_nombre_archivos_historicos()
+    df_ultimo_relevamiento = obtener_ultimo_relevamiento_cargos(listado_archivos)
+
+    df_comparacion = pd.concat([df_ultimo_relevamiento, df_funcionarios_actuales])
     
     for cargo in cargos:
         df_tmp = df_comparacion[df_comparacion.cargo == cargo]
@@ -47,4 +65,4 @@ if __name__ == "__main__":
         else:
             print("Entre {} y {} el cargo {} se mantiene sin cambios.".format(df_tmp.iloc[0].fecha, df_tmp.iloc[1].fecha, cargo))
 
-    pd.concat([df_historico, df_funcionarios_actuales]).to_csv("./data/historia_funcionarios.csv", index=False)
+    df_funcionarios_actuales.to_csv(os.path.join('.','data', datetime.date.today().isoformat() + '.csv'), index=False)
